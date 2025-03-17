e77b1df8b84b7d9044bc7fcb91fbf7607f61d7d6
Dimassis <166415819+Dimassis@users.noreply.github.com>
2025-03-17 22:47:13 +0500

    Start page

---
+++ src/test/java/sky/pro/recomendService/RecomendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.recomendService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
