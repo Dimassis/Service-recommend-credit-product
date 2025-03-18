75c2105c0928e2912ab9c47f84dc1c9c7e020ca1
DiplineRU <artgul52@gmail.com>
2025-03-18 17:06:53 +0300

    Fixed the class User (did it as in the database)

--- src/main/java/sky/pro/recomendService/model/User.java
+++ src/main/java/sky/pro/recomendService/model/User.java
@@ -1,40 +1,69 @@
 package sky.pro.recomendService.model;
 
 
-public class User {
-    private Long id;
-    private String name;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.UUID;
+@Table(name = "USERS")
+public class User {
+    @Column ("id")
+    private UUID id;
+    @Column("USERNAME")
+    private String username;
+    @Column("FIRST_NAME")
+    private String firstname;
+    @Column("LAST_NAME")
+    private String lastname;
     public User() {
     }
 
-    public User(Long id, String name) {
+    public User(UUID id, String username, String firstname, String lastname) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
                 '}';
     }
 
-    public Long getId() {
+    public UUID getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(UUID id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
