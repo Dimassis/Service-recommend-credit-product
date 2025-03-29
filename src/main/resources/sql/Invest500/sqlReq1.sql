bed7404003c5800d2b655e34eb645a73b0501366
Dimassis <166415819+Dimassis@users.noreply.github.com>
2025-03-29 16:43:37 +0500

    Add Liquibase

--- src/main/resources/sql/Invest500/sqlReq1.sql
+++
@@ -1,6 +0,0 @@
--- использует ли пользователь хотя бы один продукт типа DEBIT
-SELECT COUNT(*)
-FROM TRANSACTIONS t
-         JOIN PRODUCTS p ON t.PRODUCT_ID = p.ID
-WHERE t.USER_ID = ?
-  AND p.TYPE = 'DEBIT';
