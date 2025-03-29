7477af1af416fae820f5f8714c7770209a27970d
Dimassis <166415819+Dimassis@users.noreply.github.com>
2025-03-29 17:23:56 +0500

    Add Liquibase

--- src/main/java/sky/pro/recomendService/model/RecommendationRule.java
+++ src/main/java/sky/pro/recomendService/model/RecommendationRule.java
@@ -1,13 +1,17 @@
 package sky.pro.recomendService.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.util.UUID;
 
 @Entity
 public class RecommendationRule {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
     private String product_name;
     private UUID product_id;
